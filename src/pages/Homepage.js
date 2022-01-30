38da2b8a4744efe04f4e44b6a3c12786d44a099e
Blake T <Air@MacBooks-MacBook-Air-2.local>
2022-01-30 10:53:21 -0800

    Basic homepage layout

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -3,9 +3,13 @@ import './Homepage.css';
 
 const Homepage = () => {
     return (
-        <div>
-            <CloudIcon cloudID = '1'/>
-            <CloudIcon cloudID = '2'/>
+        <div id='homepage-grid-container'>
+            <div id='slogan-container'>
+                <h1>Knowing how to sign a card can be hard</h1>
+                <h2>Luckily, we've got you covered!</h2>
+                <h3>Discover your signature today</h3>
+                <button id='explore-button'>Explore</button>
+            </div>
         </div>
     )
 }
