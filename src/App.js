c214d123932266c0aca9c7d7801b065869efdd0f
Sterling Tamboline <stamboli@sfu.ca>
2022-05-28 09:44:15 -0700

    Hooked up nav link to signatures page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './Pages/HomePage'
 import SignaturesPage from './Pages/SignaturesPage';
 import './App.css';
 import NavBar from './Components/Navbar';
@@ -8,9 +9,10 @@ function App() {
 
   return (
       <div className="App">
-        <NavBar />
         <BrowserRouter>
+          <NavBar/>
           <Routes>
+            <Route path="/" element={<HomePage/>}/>
             <Route path="/Signatures" element={<SignaturesPage/>} />
           </Routes>
         </BrowserRouter>
