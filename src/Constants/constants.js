b91dea2d1f1c9a892cc7363a7e5602f5dce20956
Sterling Tamboline <stamboli@sfu.ca>
2022-05-27 22:03:15 -0700

    Skeleton layout for SignatureTable

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -1,4 +1,9 @@
 export const CardTypes = {
+    valentine: {
+        theme: "valentine",
+        link: "/Images/valentine.svg",
+        overlay: "Valentine's Day"
+    },
     birthday: {
         theme: "birthday",
         link: "/Images/birthday.svg",
@@ -21,3 +26,85 @@ export const CardTypes = {
     },
 
 }
+
+export const mockSignatures = [
+    {
+        occassion: "valentine",
+        type: "family",
+        message: "Happy Valentine's Day! Nothing says love like a piece of paper folded in half with some words on it."
+    },
+
+    {
+        occassion: "valentine",
+        type: "friend",
+        message: "Happy Valentine's Day! Thanks for being my person."
+    },
+
+    {
+        occassion: "valentine",
+        type: "acquaintance",
+        message: "Wishing you the sweetest, happiest Valentine's Day."
+    },
+    {
+        occassion: "valentine",
+        type: "family",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+
+]
+
+
+export const familySignatures = [
+    {
+        occassion: "valentine",
+        type: "family",
+        message: "Happy Valentine's Day! Nothing says love like a piece of paper folded in half with some words on it."
+    },
+    {
+        occassion: "valentine",
+        type: "family",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+    {
+        occassion: "valentine",
+        type: "family",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+    
+]
+
+export const friendSignatures = [
+    {
+        occassion: "valentine",
+        type: "friend",
+        message: "Happy Valentine's Day! Thanks for being my person."
+    },
+    {
+        occassion: "valentine",
+        type: "friend",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+    {
+        occassion: "valentine",
+        type: "friend",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+]
+
+export const acquaintanceSignatures = [
+    {
+        occassion: "valentine",
+        type: "acquaintance",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+    {
+        occassion: "valentine",
+        type: "acquaintance",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    },
+    {
+        occassion: "valentine",
+        type: "acquaintance",
+        message: "Wishing the sweetest, happiest Valentine's Day."
+    }
+]
