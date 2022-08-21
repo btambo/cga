76027d92eb788b8438fc76e44ab962fbc9bc24dd
Sterling Tamboline <stamboli@sfu.ca>
2022-08-21 10:56:13 -0700

    Navbar no longer overlaps on other pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,46 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import HamburgerIcon from './HamburgerIcon';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 import Logo from '../Images/logo.svg'
+import { FiMenu, FiX } from "react-icons/fi";
 
 const NavBar = () => {
 
-    const [hamburgerOpen, setHamburger] = useState(false);
+    const [menuToggle, setMenuToggle] = useState(false);
 
-    const toggleSlider = () => {
-        var slider = document.getElementById('navbar-container');
-        slider.classList.toggle('slide');
+    const handleMenuToggle = () => {
+        setMenuToggle(!menuToggle);
     }
 
-    const toggleMenu = () => {
-        setHamburger(!hamburgerOpen);
-        toggleSlider();
+    const handleCloseMenu = () => {
+        setMenuToggle(false);
     }
 
     return (
-        <nav>
-            <ul id='logo-container'>
-                <img src={Logo} alt='card' className='logo' />
-                <li className='logo'>cga</li>
-            </ul>
-            
-            <ul id='navbar-container'>
-                <Link className='navbar-element' to="/signatures">Our Signatures</Link>
-                <li className='navbar-element'>What we do</li>
-                <li className='navbar-element'>Who we are</li>
+        <nav className='navbar-container'>
+
+            <NavLink className="navbar-logo" exact to='/' onClick={handleCloseMenu}>
+                <img src={Logo} alt='card' className='navbar-logo-img' />
+                cga
+            </NavLink>
+
+            <ul className={menuToggle ? 'navbar-menu active' : 'navbar-menu'}>
+                <NavLink className="navbar-links one" exact to='/signatures' onClick={handleCloseMenu}>
+                    Our Signatures
+                </NavLink>
+
+                <NavLink className="navbar-links two" exact to='/asd' onClick={handleCloseMenu}>
+                    What we do
+                </NavLink>
+                
+                <NavLink className="navbar-links three" exact to='/asd' onClick={handleCloseMenu}>
+                    Who we are
+                </NavLink>
             </ul>
 
-            <HamburgerIcon toggleMenu={toggleMenu} />
+            <div className='menu-toggle' onClick={handleMenuToggle}>
+                {menuToggle ? <FiX/> : <FiMenu/>}
+            </div>
 
         </nav>
     )
